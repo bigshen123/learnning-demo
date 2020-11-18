d3eb425bbbf8d8eed75acc295bd4d6562b43ec63
bigshen <bigshen>
2020-11-18 20:58:25 +0800

    feat：初始导入

---
+++ target/generated-sources/annotations/com/bigshen/chatDemoService/demo/mapstruct/PersonConverterImpl.java
@@ -0,0 +1,67 @@
+package com.bigshen.chatDemoService.demo.mapstruct;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-11-15T13:11:31+0800",
+    comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_91 (Oracle Corporation)"
+)
+public class PersonConverterImpl implements PersonConverter {
+
+    @Override
+    public PersonDTO domain2dto(Person person) {
+        if ( person == null ) {
+            return null;
+        }
+
+        PersonDTO personDTO = new PersonDTO();
+
+        personDTO.setBirth( person.getBirthday() );
+        if ( person.getBirthday() != null ) {
+            personDTO.setBirthDateFormat( new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" ).format( person.getBirthday() ) );
+        }
+        Integer age = personUserAge( person );
+        if ( age != null ) {
+            personDTO.setAge( age );
+        }
+        personDTO.setId( person.getId() );
+        personDTO.setName( person.getName() );
+
+        personDTO.setBirthExpressionFormat( org.apache.commons.lang3.time.DateFormatUtils.format(person.getBirthday(),"yyyy-MM-dd HH:mm:ss") );
+
+        return personDTO;
+    }
+
+    @Override
+    public List<PersonDTO> domain2dto(List<Person> people) {
+        if ( people == null ) {
+            return null;
+        }
+
+        List<PersonDTO> list = new ArrayList<PersonDTO>( people.size() );
+        for ( Person person : people ) {
+            list.add( domain2dto( person ) );
+        }
+
+        return list;
+    }
+
+    private Integer personUserAge(Person person) {
+        if ( person == null ) {
+            return null;
+        }
+        User user = person.getUser();
+        if ( user == null ) {
+            return null;
+        }
+        Integer age = user.getAge();
+        if ( age == null ) {
+            return null;
+        }
+        return age;
+    }
+}
